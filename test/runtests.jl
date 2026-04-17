de1b3bf0aa8cfd89fab07cc0cd8fb8d86af7c175
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2026-04-17 15:58:13 +0900

    test: preserve pre-migration test suite membership

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,8 @@ using RustCall
 using ParallelTestRunner
 
 testsuite = find_tests(@__DIR__)
+# Preserve the pre-migration default suite: test_phase4.jl was not in the old harness.
+delete!(testsuite, "test_phase4")
 
 runtests(RustCall, ARGS; testsuite, init_code=quote
     using Test
