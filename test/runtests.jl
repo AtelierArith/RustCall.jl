1580f9edf0f6a5219d2448cd6baaab2dbde017fc
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2026-04-17 16:50:02 +0900

    test: finalize parallel runner fallback handling

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,22 +4,32 @@ using ParallelTestRunner
 args = parse_args(ARGS)
 testsuite = find_tests(@__DIR__)
 serial_testsuite = Dict{String, Expr}()
+serial_test_names = ("test_cache", "test_core_api")
+
+for test_name in collect(keys(testsuite))
+    basename = split(test_name, '/')[end]
+    if !startswith(basename, "test_")
+        delete!(testsuite, test_name)
+    end
+end
+
+if args.list !== nothing
+    println("Available tests:")
+    for test in sort(collect(keys(testsuite)))
+        println(" - $test")
+    end
+    exit(0)
+end
 
 if filter_tests!(testsuite, args)
     # Preserve the pre-migration default suite: test_phase4.jl was not in the old harness.
     delete!(testsuite, "test_phase4")
 end
 
-if haskey(testsuite, "test_cache")
-    serial_testsuite["test_cache"] = pop!(testsuite, "test_cache")
-end
-
-if args.list !== nothing
-    println("Available tests:")
-    for test in keys(merge(copy(testsuite), serial_testsuite))
-        println(" - $test")
+for test_name in serial_test_names
+    if haskey(testsuite, test_name)
+        serial_testsuite[test_name] = pop!(testsuite, test_name)
     end
-    exit(0)
 end
 
 init_code = quote
@@ -27,18 +37,34 @@ init_code = quote
     using RustCall
 end
 
+parallel_error = Ref{Any}(nothing)
+serial_error = Ref{Any}(nothing)
+
 if !isempty(testsuite)
-    runtests(RustCall, args; testsuite, init_code)
+    try
+        runtests(RustCall, args; testsuite, init_code)
+    catch err
+        parallel_error[] = err
+    end
 end
 
 if !isempty(serial_testsuite)
-    serial_args = ParallelTestRunner.ParsedArgs(
-        Some(1),
-        args.verbose,
-        args.quickfail,
-        nothing,
-        args.custom,
-        args.positionals,
-    )
-    runtests(RustCall, serial_args; testsuite=serial_testsuite, init_code)
+    serial_argv = String["--jobs=1"]
+    args.verbose !== nothing && push!(serial_argv, "--verbose")
+    args.quickfail !== nothing && push!(serial_argv, "--quickfail")
+    append!(serial_argv, args.positionals)
+    serial_args = parse_args(serial_argv)
+    try
+        runtests(RustCall, serial_args; testsuite=serial_testsuite, init_code)
+    catch err
+        serial_error[] = err
+    end
+end
+
+if parallel_error[] !== nothing && serial_error[] !== nothing
+    throw(CompositeException(Any[parallel_error[], serial_error[]]))
+elseif parallel_error[] !== nothing
+    throw(parallel_error[])
+elseif serial_error[] !== nothing
+    throw(serial_error[])
 end
