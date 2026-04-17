0f30a1b3bb0858d1a39a2e8265b8bd248073bc0f
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2026-04-17 16:03:19 +0900

    test: preserve explicit phase4 selection

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,15 @@
 using RustCall
 using ParallelTestRunner
 
+args = parse_args(ARGS)
 testsuite = find_tests(@__DIR__)
-# Preserve the pre-migration default suite: test_phase4.jl was not in the old harness.
-delete!(testsuite, "test_phase4")
 
-runtests(RustCall, ARGS; testsuite, init_code=quote
+if filter_tests!(testsuite, args)
+    # Preserve the pre-migration default suite: test_phase4.jl was not in the old harness.
+    delete!(testsuite, "test_phase4")
+end
+
+runtests(RustCall, args; testsuite, init_code=quote
     using Test
     using RustCall
 end)
