b0e238186ce41a456e683c74c86e43ca677f26b9
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2026-04-17 16:25:17 +0900

    test: run cache suite serially

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,13 +3,42 @@ using ParallelTestRunner
 
 args = parse_args(ARGS)
 testsuite = find_tests(@__DIR__)
+serial_testsuite = Dict{String, Expr}()
 
 if filter_tests!(testsuite, args)
     # Preserve the pre-migration default suite: test_phase4.jl was not in the old harness.
     delete!(testsuite, "test_phase4")
 end
 
-runtests(RustCall, args; testsuite, init_code=quote
+if haskey(testsuite, "test_cache")
+    serial_testsuite["test_cache"] = pop!(testsuite, "test_cache")
+end
+
+if args.list !== nothing
+    println("Available tests:")
+    for test in keys(merge(copy(testsuite), serial_testsuite))
+        println(" - $test")
+    end
+    exit(0)
+end
+
+init_code = quote
     using Test
     using RustCall
-end)
+end
+
+if !isempty(testsuite)
+    runtests(RustCall, args; testsuite, init_code)
+end
+
+if !isempty(serial_testsuite)
+    serial_args = ParallelTestRunner.ParsedArgs(
+        Some(1),
+        args.verbose,
+        args.quickfail,
+        nothing,
+        args.custom,
+        args.positionals,
+    )
+    runtests(RustCall, serial_args; testsuite=serial_testsuite, init_code)
+end
